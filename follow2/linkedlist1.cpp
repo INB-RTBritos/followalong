bc1d7628f0f98a90906a1e36eef58952c8b0a7b6
INB-RTBritos <154307214+INB-RTBritos@users.noreply.github.com>
2024-05-10 17:28:25 +0800

    commit changes

--- follow2/linkedlist1.cpp
+++ follow2/linkedlist1.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 struct Node {
     int value; // Data member
-    Node *next; // next pointer
-}*head = NULL, *current = NULL;
+    Node *next;// next pointer 
+}*head = NULL, *current = NULL, *previous = NULL;
 
 // Prototype for linked list operation
 void AppendNode(int);
+void DisplayList();
+void InsertNode(int);
+
 
 int main() {
     int val;
@@ -16,8 +19,9 @@ int main() {
         cout << "Enter a value: ";
         cin >> val;
         if (val < 0) break;
-        AppendNode(val);
+        InsertNode(val);
     } while (true);
+    DisplayList();
 }
 
 // Function Definition
@@ -42,5 +46,53 @@ void AppendNode(int newValue) {
         }
         current->next = newNode; // if last node is reached, append the new node
     }
-    cout << "New node is appended...";
+    cout << "\nNew node is appended...\n\n";
+}
+
+void DisplayList() {
+
+    if (!head) {
+        cout << "\n\nThe list is empty\n\n";
+    }
+    else {
+        // traverse the list up to the last node
+        current = head;
+        while (current) { // same as current != NULL
+            cout << current->value << "\n";
+            // moves to the next node
+            current = current->next;
+        }
+        cout << "\nEnd of list...\n\n";
+    }
+}
+
+void InsertNode(int newValue) {
+    Node *newNode = new Node; // Create a new Node
+    newNode->value = newValue; // Store data to the data member
+    newNode->next = NULL; // Set the next pointer to NULL
+
+    if (!head) {
+        head = newNode;
+    } 
+    else {
+        // traverse the list to find the correct position where the new node will be inserted
+        current = head;
+        // loop will stop if the new value is lesser than the current node
+        // or the end of the list is reached
+        while (current && current->value < newValue) {
+            previous = current;
+            current = current->next;
+        }
+        // insert node
+        if (!previous) {
+            head = newNode;
+            newNode->next = current;
+        }
+        else {
+            // break the link so that the new node can be inserted
+            previous->next = newNode;
+            // reconnect the link
+            newNode->next = current;
+        }
+    }
 }
