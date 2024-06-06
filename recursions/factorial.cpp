690d2f5e90771487ccb6fbe53594977dbe89f995
INB-RTBritos <154307214+INB-RTBritos@users.noreply.github.com>
2024-06-06 22:03:06 +0800

    commit changes

--- recursions/factorial.cpp
+++ recursions/factorial.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int Factorial(int);
+int Factorial(int); // function declaration
+                    // declaring as int to make the function recursion call become false at some point
 
 int main() {
-    int numHolder;
-    int factorial;
+    int numHolder; // number from main to be passed as parameter to the function
+    int factorial; 
 
     cout << "Input a number to calculate its factorial: ";
     cin >> numHolder;
 
-    factorial = Factorial(numHolder);
+    factorial = Factorial(numHolder); // putting the functions returned value to the variable to display
     cout << "The Factorial of " << numHolder << " is: " << factorial;
 }
 
 int Factorial(int num){
-    if (num == 0) 
+    if (num == 0) // will be the condition to make the recursive call stop
     return 1;
 
-    return num * Factorial(num - 1);
+    return num * Factorial(num - 1); // cannot be declared as num-- because the parameter expects an int so you
+                                    // need to use something of similar variable type               
+                                    //  returning the num but uses the value of the function to be part of the 
+                                    //  formula and eventually goes to first condition one it hits zero
+                                     
 }
